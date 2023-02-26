861f682059f7ded319e08bb5a1ab9bab38df9144
Shriya <sh.gundamaraju@gmail.com>
2023-02-26 01:23:41 -0500

    hhh

--- App.js
+++ App.js
@@ -83,7 +83,6 @@ export default function MyStack() {
 
 }
 
-
 const HomeScreen = ({navigation}) => {
   const [selected, setSelected] = React.useState("");
   
@@ -101,7 +100,7 @@ const HomeScreen = ({navigation}) => {
         <StatusBar style="auto" />
         <SelectList 
           setSelected={(val) => setSelected(val)} 
-          onSelect
+          // onSelect = {}
           font-family='cochin'
           data={data} 
           boxStyles={{borderRadius: 9}}
@@ -111,7 +110,7 @@ const HomeScreen = ({navigation}) => {
           title="Get my mix!"
           
           onPress={() =>
-            navigation.navigate('Mood Page')
+            navigation.navigate('Mood Page', {paramKey: selected})
           }
           color="#841584"
         />
@@ -146,24 +145,199 @@ const styles = StyleSheet.create({
   },
 });
 
-const MoodScreen = ({navigation}) => {
-  return (
+const randomInt = () => {
+  return Math.floor(Math.random() * 6);
+}
+
+const happy = () => {
+  const happyArr = ['https://open.spotify.com/embed/playlist/37i9dQZF1DX3rxVfibe1L0',
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX7KNKjOK0o75', 
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DWYBO1MoTDhZI',
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX1uG5byNIgDA',
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX2sUQwD7tbmL',
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DXcBWIGoYBM5M?']
+  const playlist = happyArr[randomInt()];
+  return(
     <>
     <View>
-      <WebView source ={{uri: 'https://open.spotify.com/embed/playlist/37i9dQZF1DXcBWIGoYBM5M'}}/>
+      <WebView source ={{uri: playlist}}/>
     </View>
-    <View style={{ flex: 1 }}>
+    <View style={{flex: 1, marginBottom: 200, marginTop: 200}}>
     <WebView
       automaticallyAdjustContentInsets={false}
-      source={{ uri: 'https://open.spotify.com/embed/playlist/37i9dQZF1DXcBWIGoYBM5M' }}
+      source={{ uri: playlist}}
       javaScriptEnabled={true}
       domStorageEnabled={true}
       decelerationRate="normal"
       startInLoadingState={true}
-      scalesPageToFit={true}
+      scalesPageToFit={false}
     />
   </View>
   </>  
+  )
+}
 
-    );
+const romantic = () => {
+  const romanticArr = ['https://open.spotify.com/embed/playlist/37i9dQZF1DXbEm2sKzgoJ8',                                                           
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX6mvEU1S6INL',                                                            
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DWTbzY5gOVvKd',                                                             
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX38lOuCWlLV1',                                                            
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DWVGy1YP1ojM5',                                                            
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DXcbAIldMQMIs']
+  const playlist = romanticArr[randomInt()];
+  return(
+    <>
+    <View>
+      <WebView source ={{uri: playlist}}/>
+    </View>
+    <View style={{flex: 1, marginBottom: 200, marginTop: 200}}>
+    <WebView
+      automaticallyAdjustContentInsets={false}
+      source={{ uri: playlist}}
+      javaScriptEnabled={true}
+      domStorageEnabled={true}
+      decelerationRate="normal"
+      startInLoadingState={true}
+      scalesPageToFit={false}
+    />
+  </View>
+  </>  
+  )
+}
+
+const sad = () => {
+  const sadArr = ['https://open.spotify.com/embed/playlist/37i9dQZF1DX7qK8ma5wgG1',                                                          
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX3YSRoSdA634',                                                           
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX59NCqCqJtoH',                                                          
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DWVV27DiNWxkR',                                                         
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DWX83CujKHHOn',                                                            
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX9LT7r8qPxfa']
+  const playlist = sadArr[randomInt()];
+  return(
+    <>
+    <View>
+      <WebView source ={{uri: playlist}}/>
+    </View>
+    <View style={{flex: 1, marginBottom: 200, marginTop: 200}}>
+    <WebView
+      automaticallyAdjustContentInsets={false}
+      source={{ uri: playlist}}
+      javaScriptEnabled={true}
+      domStorageEnabled={true}
+      decelerationRate="normal"
+      startInLoadingState={true}
+      scalesPageToFit={false}
+    />
+  </View>
+  </>  
+  )
+}
+
+const relaxed = () => {
+  const relaxedArr = ['https://open.spotify.com/embed/playlist/37i9dQZF1DX0MLFaUdXnjA',                                                          
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX52ln8eMkne9',                                                            
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX6tTW0xDxScH',                                                            
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX889U0CL85jj',                                                            
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX6VdMW310YC7',                                                            
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DXa3NnZWk6Z3T']
+  const playlist = relaxedArr[randomInt()];
+  return(
+    <>
+    <View>
+      <WebView source ={{uri: playlist}}/>
+    </View>
+    <View style={{flex: 1, marginBottom: 200, marginTop: 200}}>
+    <WebView
+      automaticallyAdjustContentInsets={false}
+      source={{ uri: playlist}}
+      javaScriptEnabled={true}
+      domStorageEnabled={true}
+      decelerationRate="normal"
+      startInLoadingState={true}
+      scalesPageToFit={false}
+    />
+  </View>
+  </>  
+  )
+}
+
+const hype = () => {
+  const hypeArr = ['https://open.spotify.com/embed/playlist/37i9dQZF1DXa2PvUpywmrr',         
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DWY4xHQp97fN6',                                                            
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX8ky12eWIvcW',                                                           
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX5I05jXm1F2M',                                                            
+  'https://open.spotify.com/embed/playlist/37i9dQZF1EIhpG2qPPV1Lr',                                                            
+  'https://open.spotify.com/embed/playlist/3Qlo8PGJKE53FgTcIjuIvJ']
+  const playlist = hypeArr[randomInt()];
+  return(
+    <>
+    <View>
+      <WebView source ={{uri: playlist}}/>
+    </View>
+    <View style={{flex: 1, marginBottom: 200, marginTop: 200}}>
+    <WebView
+      automaticallyAdjustContentInsets={false}
+      source={{ uri: playlist}}
+      javaScriptEnabled={true}
+      domStorageEnabled={true}
+      decelerationRate="normal"
+      startInLoadingState={true}
+      scalesPageToFit={false}
+    />
+  </View>
+  </>  
+  )
+}
+
+const studious = () => {
+  const studiousArr = ['https://open.spotify.com/embed/playlist/0vvXsWCC9xrXsKd4FyS8kM',                                                           
+  'https://open.spotify.com/embed/playlist/2OetfhEdq7for0HjLQIMC6',                                                            
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX8NTLI2TtZa6',                                                 
+  'https://open.spotify.com/embed/playlist/156kA62wcOrCknkCFG073f',                                                            
+  'https://open.spotify.com/embed/playlist/27Zm1P410dPfedsdoO9fqm',                                                            
+  'https://open.spotify.com/embed/playlist/37i9dQZF1DX692WcMwL2yW']
+  const playlist = studiousArr[randomInt()];
+  return(
+    <>
+    <View>
+      <WebView source ={{uri: playlist}}/>
+    </View>
+    <View style={{flex: 1, marginBottom: 200, marginTop: 200}}>
+    <WebView
+      automaticallyAdjustContentInsets={false}
+      source={{ uri: playlist}}
+      javaScriptEnabled={true}
+      domStorageEnabled={true}
+      decelerationRate="normal"
+      startInLoadingState={true}
+      scalesPageToFit={false}
+    />
+  </View>
+  </>  
+  )
+}
+
+const ifElse = (selected) => {
+  if (selected == 'Happy') {
+    return happy();
+  } else if (selected == 'Romantic'){
+    return romantic();
+  } else if (selected == 'Sad'){
+    return sad();
+  } else if (selected == 'Relaxed'){
+    return relaxed();
+  }else if(selected == 'Hype'){
+    return hype();
+  }else if(selected == 'Studious'){
+    return studious(); 
+  }
+}
+
+const MoodScreen = ({route, navigation}) => {
+  const {paramKey} = route.params;
+  return(
+      <>
+      {ifElse(paramKey)}
+      </>
+  );
 };
