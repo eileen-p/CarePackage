2aee443c374eab649bc6c2d0088292aa84ce4fcd
amotupalli7 <sspmam@gmail.com>
2023-02-25 20:06:07 -0500

    transition

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Alert, Pressable } from 'react-native';
 import React, { useState } from 'react';
-//import * as React from 'react';
-
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 //import MoodPage from './moodpage';
 
 import { SelectList } from 'react-native-dropdown-select-list';
+import { CardStyleInterpolators, TransitionSpecs } from '@react-navigation/stack';
 // Used dropdown-select-list from https://www.npmjs.com/package/react-native-dropdown-select-list
 
 const Stack = createNativeStackNavigator();
 
 export default function MyStack() {
+  const customTransition = {
+    gestureEnabled: true,
+    gestureDirection: 'vertical',
+    transitionSpec: {
+      open: TransitionSpecs.TransitionIOSSpec,
+      close: TransitionSpecs.TransitionIOSSpec,
+    },
+    cardStyleInterpolators: ({current,next,layouts}) => {
+      return { 
+        cardStyle: {
+          transform: [
+            {
+              translateX: current.progress.interpolate({
+                inputRange: [0,1],
+                outputRange: [layouts.screen.width,0],
+              })
+            },
+            {
+              rotate: current.progress.interpolate({
+                inputRange: [0,1],
+                outputRange: ["180deg","0deg"],
+              }),
+
+            },
+            {
+              scale: next ?
+                next.progress.interpolate({
+                  inputRange: [0,1],
+                  outputRange: [1,0.7],
+                }) : 1
+            }
+          ]
+        }
+      }
+    }
+    }
+  
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+      screenOptions={{
+        ...customTransition,
+        // gestureEnabled: true,
+        // gestureDirection: 'horizontal',
+        // transitionSpec:{ 
+        //   open: TransitionSpecs.TransitionIOSSpec,
+        //   close: TransitionSpecs.TransitionIOSSpec,
+        // },
+        // cardStyleInterpolator: CardStyleInterpolators.forModalPresentationIOS,
+      }}
+      options = {{...customTransition}}
+      >
         <Stack.Screen
           name= "Home"
           component= {HomeScreen}
